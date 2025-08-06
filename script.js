0c32726fe5f5df5f60c14636ea344fae0e7dbbd4
toia08 <vitoria.zielinski@escola.pr.gov.br>
2025-08-06 09:46:47 -0300

    PERGUNTAS

--- script.js
+++ script.js
@@ -4,20 +4,80 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas=[
+const perguntas = [
     {
-        enunciado:quem foi,
-        alternativa:[
-            { texto: correta,
-                afirmação: resposta correta,
-                pontos: 1
+        enunciado:"Qual é o objetivo principal de um lutador de boxe durante uma luta?",
+        alternativas:[
+            {
+                texto:"Derrubar ou pontuar mais que o adversário",
+                afirmação:"resposta correta",
+                pontos:1
             },
-            { texto: errada,
-                afirmação: resposta errada,
-                pontos: 0,
+            {
+                texto:"Impedir o adversário de se mover com chutes",
+                afirmação:"resposta errada",
+                pontos:0
+            }
+        ]
+    },
+     {
+        enunciado:"Qual é o nome do espaço onde ocorrem as lutas de boxe?",
+        alternativas:[
+            {
+                texto:"Ringue",
+                afirmação:"resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Tatame",
+                afirmação:"resposta errada",
+                pontos:0
+            }
+        ]
+    },
+      {
+        enunciado:"Quantos rounds geralmente tem uma luta profissional de boxe?",
+        alternativas:[
+            {
+                texto:"12 rounds",
+                afirmação:"resposta correta",
+                pontos:1
+            },
+            {
+                texto:"5 rounds",
+                afirmação:"resposta errada",
+                pontos:0
+            }
+        ]
+    },
+     {
+        enunciado:" Qual parte do corpo é usada para atacar no boxe?",
+        alternativas:[
+            {
+                texto:"Os punhos",
+                afirmação:"resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Os pés",
+                afirmação:"resposta errada",
+                pontos:0
+            }
+        ]
+    },
+     {
+        enunciado:"Qual destes equipamentos é obrigatório no boxe?",
+        alternativas:[
+            {
+                texto:"Luvas de boxe",
+                afirmação:"resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Espada",
+                afirmação:"resposta errada",
+                pontos:0
             }
         ]
     },
 ]
-
-
