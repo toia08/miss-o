044914cd27edf91da3d9023ae85ba7067b77430c
toia08 <vitoria.zielinski@escola.pr.gov.br>
2025-08-18 12:28:30 -0300

    oi

--- script.js
+++ script.js
@@ -81,3 +81,53 @@ const perguntas = [
         ]
     },
 ]
+
+let atual = 0; 
+let perguntaAtual; 
+let historiaFinal = ""; 
+let pontos = 0;  
+
+function mostraPergunta(){ 
+    perguntaAtual = perguntas[atual]; 
+    caixaPerguntas.textContent = perguntaAtual.enunciado; 
+    caixaAlternativas.textContent = ""; 
+    mostraAlternativas(); 
+}
+
+function mostraAlternativas(){ 
+    for(const alternativa of perguntaAtual.alternativas){ 
+        const botaoAlternativas = document.createElement("button"); 
+        botaoAlternativas.textContent = alternativa.texto; 
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa)); 
+        caixaAlternativas.appendChild(botaoAlternativas); 
+    }
+}
+
+function respostaSelecionada(alternativa) { 
+    const afirmacao = alternativa.afirmacao; 
+    historiaFinal = afirmacao; 
+    pontos += alternativa.pontos;  
+    atual++; 
+
+    if (atual < perguntas.length) { 
+        mostraPergunta();  
+    } else {
+        exibeResultado();  
+    }
+}
+
+function exibeResultado() { 
+    caixaPerguntas.textContent = "Fim do Quiz!"; 
+    caixaAlternativas.textContent = "";  
+    textoResultado.textContent = `Sua pontuação final é: ${pontos} pontos.`; 
+
+    if (pontos === perguntas.length) { 
+        textoResultado.textContent += " Parabéns! Você acertou todas as questões!"; 
+    } else if (pontos > perguntas.length / 2) { 
+        textoResultado.textContent += " Bom trabalho, você teve um desempenho legal!"; 
+    } else { 
+        textoResultado.textContent += " Você pode melhorar! Tente novamente!"; 
+    }
+}
+
+mostraPergunta();
